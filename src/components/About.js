4337cf3734b36d17e7df3a1166b89394169ecd29
Amogh Upadhyaya <amoghu26@gmail.com>
2021-07-19 22:26:26 -0700

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-white">
-            Hi, my name is Amogh and I am an aspiring CS Major
+            Hello, my name is Amogh and I am an aspiring CS Major
           </h1>
           <p className="mb-8 leading-relaxed text-black">
             I am an 11th grader attending Monta Vista High School in 
