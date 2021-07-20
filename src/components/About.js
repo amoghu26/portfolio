853cd4f83166a4d8582df205e88fd24c56024f51
Amogh Upadhyaya <amoghu26@gmail.com>
2021-07-19 22:22:51 -0700

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-white">
-            Hi, I'm Amogh.
+            Hi, I'm Amogh and
             <br className="hidden lg:inline-block" />
             I am an aspiring CS Major
           </h1>
