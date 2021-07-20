4ea21a97322530d2534c39e08239580f339e62de
Amogh Upadhyaya <amoghu26@gmail.com>
2021-07-19 21:59:17 -0700

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
-            Amogh Upadhyaya
+            Amogh Upadhyaya's Portfolio
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-black	flex flex-wrap items-center text-base justify-center">
@@ -26,7 +26,6 @@ export default function Navbar() {
           href="#contact"
           className="inline-flex text-black items-center bg-white border-0 py-1 px-3 focus:outline-none hover:bg-white rounded text-base mt-4 md:mt-0">
           Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
     </header>
