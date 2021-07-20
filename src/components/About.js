b37054446fe6f0d92bee1727d0a5a6bb81425528
Amogh Upadhyaya <amoghu26@gmail.com>
2021-07-19 22:40:33 -0700

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,3 @@
-// src/components/About.js
 
 import React from "react";
 
@@ -8,7 +7,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-white">
-            Hello, my name is Amogh and I am an aspiring CS Major
+            Hi, my name is Amogh and I am an aspiring Computer Science Major
           </h1>
           <p className="mb-8 leading-relaxed text-black">
             I am an 11th grader attending Monta Vista High School in 
@@ -30,8 +29,8 @@ export default function About() {
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            alt="Picture of sunset"
+            src="./sunset.jpg"
           />
         </div>
       </div>
