035c8baea532f4cdec614c2d9489daf480fb7bfd
Amogh Upadhyaya <amoghu26@gmail.com>
2021-08-05 18:09:54 -0700

    Add files via upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -40,6 +40,42 @@ export default function Contact() {
             src="https://www.google.com/maps/embed/v1/place?q=cupertino,+california&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
         </div>
+        <form
+          netlify
+          name="contact"
+          onSubmit={handleSubmit}
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Work With Me
+          </h2>
+          <p className="leading-relaxed mb-5 text-gray-400">
+            My location can be seen to the left. If you are interested in working with me and have an opportunity close by, please directly email me or fill out this contact form and I will get back to you ASAP.
+          </p>
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1 text-indigo-400">
+                22883 Longdown Road <br />
+                Cupertino, CA 95014
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                amoghu26@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed text-indigo-400">408-660-0793</p>
+            </div>
+          </div>
+          
+        </form>
       </div>
     </section>
   );
