45b8338fd0b0fb4ec18ba295ffc9cce518544a8b
Amogh Upadhyaya <amoghu26@gmail.com>
2021-09-04 12:07:30 -0700

    Add files via upload

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,3 @@
-import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
 import { skills } from "../data";
 
@@ -7,7 +6,6 @@ export default function Skills() {
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Industry Knowledge &amp; Skills
           </h1>
